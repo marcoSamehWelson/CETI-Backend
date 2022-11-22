18a2d571e0b81d83cf8cdaec1b10d3067bde0eab
marcoSamehWelson <marcosamehwelson@gmail.com>
2022-11-22 02:53:55 +0200

    Add SessionReservation cs

---
+++ Caritas_Egypt_Backend/Models/SessionReservationcs.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using System;
+
+namespace Caritas_Egypt_Backend.Models
+{
+ 
+    public class SessionReservationcs
+    {
+        public SessionReservationcs()
+        {
+            Id = Guid.NewGuid();
+        }
+        [Required]
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid? coursePriceId { get; set; }
+        public CoursePrice coursePrice { get; set; }
+        public Guid? StudentId { get; set; }
+        public Student student { get; set; }
+        public Guid? TrainerId { get; set; }
+        public Trainer trainer { get; set; }
+        [Display(Name = "السعر")]
+        public string Price { get; set; }
+
+    }
+}
