772bfa1e9765281d0ba118d1df8e1dfb72579d7a
pc <pc@DESKTOP-M0VFFHC>
2023-05-17 16:54:30 +0200

    commit

---
+++ Caritas_Egypt_Backend/Models/JobFE.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using iTextSharp.text.pdf.qrcode;
+
+namespace Caritas_Egypt_Backend.Models
+{
+    public class JobFE
+    {
+        public JobFE()
+        {
+            Id = Guid.NewGuid();
+
+        }
+        [Required]
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "العنوان")]
+        public string Tittle { get; set; }
+        [StringLength(5000)]
+        [Display(Name = "الوصف")]
+        public string Descreption { get; set; }
+        [Display(Name = "الصورة")]
+        public string img { get; set; }
+
+    }
+}
+
---
+++ Caritas_Egypt_Backend/Models/TrainingFE.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Caritas_Egypt_Backend.Models
+{
+    public class TrainingFE
+    {
+
+            public TrainingFE()
+            {
+                Id = Guid.NewGuid();
+
+            }
+            [Required]
+            [Key]
+            public Guid Id { get; set; }
+            [Required]
+            [StringLength(50)]
+            [Display(Name = "العنوان")]
+            public string Tittle { get; set; }
+            [StringLength(5000)]
+            [Display(Name = "الوصف")]
+            public string Descreption { get; set; }
+            [Display(Name = "الصورة")]
+            public string img { get; set; }
+
+        }
+    }
